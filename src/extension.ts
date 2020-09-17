1db5c192503e17bcdaa7db5d9212585d73c24571
wutai <1615140542@qq.com>
2020-09-17 09:54:17 +0800

    generate .yaml after create configuration folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
   let createCodenoteConfig = vscode.commands.registerCommand(
@@ -6,10 +7,19 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       const folders = vscode.workspace.workspaceFolders;
       const root = folders?.[0].uri.fsPath;
+      const folder = `${root}\\.conoco`;
       if (typeof root !== 'undefined') {
-        vscode.workspace.fs.createDirectory(
-          vscode.Uri.file(`${root}\\.conoco`)
-        );
+        vscode.workspace.fs
+          .createDirectory(vscode.Uri.file(folder))
+          .then(() => {
+            fs.writeFile(`${folder}\\history.yaml`, '', 'utf8', (err) => {
+              if (err) {
+                vscode.window.showErrorMessage(
+                  err + '\nIt seem that can not create the history configuration file.'
+                );
+              }
+            });
+          });
       } else {
         vscode.window.showErrorMessage(
           'Failed to create the codenote configuration file.'
